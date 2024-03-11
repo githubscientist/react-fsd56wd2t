dcfc3a868d22971cf7258087a817d30e35c4bf56
githubscientist <sathish.datascientist@gmail.com>
2024-03-11 21:31:28 +0530

    external css

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,14 +1,10 @@
+import './Hello.css';
+
 // creating a second component: Hello
 const Hello = (props) => {
 
-    // javascript codes
-    // console.log(props);
-    const headerStyle = {
-        color: 'green', fontFamily: 'monospace'
-    };
-
   return (
-      <h1 style={headerStyle}>Hello { props.name }!</h1>
+      <h1 className="header">Hello { props.name }!</h1>
   )
 } 
 
