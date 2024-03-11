0b8d325a81a8a68004e87fe97bd120e5466c68a3
githubscientist <sathish.datascientist@gmail.com>
2024-03-11 21:39:03 +0530

    import bootstrap --index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import Hello from "./components/Hello"; // import the Hello component
+import './App.css'; // import the App.css file
 
 // Creating our first component
 const App = () => {
   return (
-    <div>
+    <div className="d-flex flex-column justify-content-center align-items-center">
       <Hello name="React" />
       <Hello name="CSS" />
       <Hello name="HTML" />
