879a1904b4fea9643b7dc19667e7b06691d4d348
githubscientist <sathish.datascientist@gmail.com>
2024-03-12 20:59:25 +0530

    lists using forEach

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ const App = (props) => {
 
   console.log(props.notes);
 
+  let notesList = [];
+
+  props.notes.forEach(note => {
+    notesList.push(<li>{note.content}</li>);
+  });
+
+  console.log(notesList);
+
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        <li>{props.notes[0].content}</li>
-        <li>{props.notes[1].content}</li>
-        <li>{props.notes[2].content}</li>
+        { notesList }
       </ul>
     </div>
   )
