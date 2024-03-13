bd9f88768d48e5de5aa8d0000134f1d4abaeeac2
githubscientist <sathish.datascientist@gmail.com>
2024-03-13 20:04:56 +0530

    default props

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -6,10 +6,16 @@ class Hello extends Component {
     render() {
         console.log(this.props);
 
+        // const { name } = this.props.name ? this.props : { name: 'Guest' };
+
         return (
             <p>Hello { this.props.name }!</p>
         )
   }
+}
+
+Hello.defaultProps = {
+    name: 'Guest',
 }
 
 export default Hello;
