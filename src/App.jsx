490f29d40cac9d023f0ccc3ff8653108972e1a9c
githubscientist <sathish.datascientist@gmail.com>
2024-03-14 20:46:44 +0530

    multiple states

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,21 @@
-import { useState } from "react";
-import Display from "./components/Display";
-import Button from "./components/Button";
+import React, { useState } from 'react';
 
 const App = () => {
 
-  const [count, setCount] = useState(0);
+  const [left, setLeft] = useState(0);
+  const [right, setRight] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
+  const handleLeftClick = () => {
+    setLeft(left + 1);
   }
 
-  const decrement = () => {
-    setCount(count - 1);
+  const handleRightClick = () => {
+    setRight(right + 1);
   }
-
-  const reset = () => {
-    setCount(0);
-  }
-
-  console.log('App component rendering...', count);
 
   return (
     <div>
-      <Display count={ count } />
-      <Button
-        text="Increment" 
-        onClick={ increment }
-      />
-      <Button
-        text="Decrement" 
-        onClick={decrement}
-      />
-      <Button
-        text="Reset" 
-        onClick={reset}
-      />
+      <p>{ left } <button onClick={handleLeftClick}>left</button> <button onClick={handleRightClick}>right</button> { right }</p>
     </div>
   )
 }
