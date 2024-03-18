af0d2fb642d05ebc467c4c9f19290df05badf5fe
githubscientist <sathish.datascientist@gmail.com>
2024-03-18 21:26:09 +0530

    email otp

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,6 @@
-// import guvi from './assets/guvi.jpg';
-
 const App = () => {
   return (
-    <div>
-      <h2>Hello, Guvi!!</h2>
-      <img
-        src="https://images.unsplash.com/photo-1683009427540-c5bd6a32abf6?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt="guvi" 
-        width="100%"
-      />
-    </div>
+    <div>Email OTP Example</div>
   )
 }
 
