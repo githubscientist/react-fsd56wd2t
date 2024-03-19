385ce97a5ed0948db8d981520b61a495ac91f533
githubscientist <sathish.datascientist@gmail.com>
2024-03-19 19:02:06 +0530

    add todo example

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,51 @@
-const App = () => {
+import { useEffect, useState } from "react";
+
+const App = ({ todoList }) => {
+
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    setTodos(todoList);
+  }, []);
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  const addTodo = (e) => {
+    e.preventDefault();
+    // console.log(e.target.newTodo.value);
+    const newTodo = {
+      id: todos.length + 1,
+      title: e.target.newTodo.value,
+      completed: false
+    }
+
+    setTodos([...todos, newTodo]);
+
+    // clear the input
+    e.target.newTodo.value = '';
+
+    // set the focus back to the input
+    e.target.newTodo.focus();
+  }
+
   return (
-    <div>Email OTP Example</div>
+    <div>
+      <h1>Todo List</h1>
+      <ul>
+        {
+          todos.map((todo) => (
+            <li key={todo.id}>{ todo.title }</li>
+          ))
+        }
+      </ul>
+
+      <form onSubmit={addTodo}>
+        <input type="text" name="newTodo" required />
+        <button type="submit">Add</button>
+      </form>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -6,4 +6,22 @@ import './index.css'
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap/dist/js/bootstrap.bundle.js';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const todos = [
+    {
+        id: 1,
+        title: 'Learn React',
+        completed: false
+    },
+    {
+        id: 2,
+        title: 'Learn TypeScript',
+        completed: false
+    },
+    {
+        id: 3,
+        title: 'Learn GraphQL',
+        completed: false
+    }
+];
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App todoList={ todos } />);
