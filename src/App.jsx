2f21d820a4f460eabfbab166974c17e00b64c1d1
githubscientist <sathish.datascientist@gmail.com>
2024-03-19 20:58:16 +0530

    routing revisited with modern way of representation

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./components/Home";
 import Notes from "./components/Notes";
-import Users from "./components/Users";
-import { Link, Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/notes',
+    element: <Notes />
+  }
+]);
 
 const App = () => {
-
-  const navStyles = {
-    display: 'flex',
-    justifyContent: 'space-around',
-    padding: '1rem',
-    borderBottom: '1px solid #ddd'
-  }
-
-  return (
-    <Router>
-      <div style={navStyles}>
-        <Link to="/">home</Link>
-        <Link to="/notes">notes</Link>
-        <Link to="/users">users</Link>
-      </div>
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/notes" element={<Notes />} />
-        <Route path="/users" element={<Users />} />
-      </Routes>
-
-      <div>
-        <i>Notes Application, Guvi-2024 &copy; All rights reserved</i>
-      </div>
-    </Router>
-  )
+  return <RouterProvider router={router} />
 }
 
 export default App;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Home = () => {
+
+  const navStyles = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    padding: '1rem',
+    borderBottom: '1px solid #ddd'
+  }
+
   return (
     <div>
-        <h1>Home</h1>
+      <div style={navStyles}>
+        <Link to="/">home</Link>
+        <Link to="/notes">notes</Link>
+        <Link to="/users">users</Link>
+      </div>
+      
+      <div>
+        <i>Notes Application, Guvi-2024 &copy; All rights reserved</i>
+      </div>
     </div>
   )
 }
