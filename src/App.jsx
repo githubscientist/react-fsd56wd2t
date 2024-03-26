532c0863274f62910c335dad11f158cecfab752e
githubscientist <sathish.datascientist@gmail.com>
2024-03-26 19:17:02 +0530

    button click with array state

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,64 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Button from './components/Button';
 
 const App = () => {
 
-  const [value, setValue] = useState(null);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  useEffect(() => {
+    console.log(squares);
+  }, [squares]);
+
+  const handleClick = (index) => {
+    console.log(index + ' clicked');
+  }
 
   return (
     <div>
       <div className='row'>
         <Button
-          value={value} 
-          onClick={() => setValue('X')}
+          value={squares[0]}
+          onClick={() => handleClick(0)}
         />
-        <Button />
-        <Button />
+        <Button
+          value={squares[1]}
+          onClick={() => handleClick(1)}
+        />
+        <Button
+          value={squares[2]}
+          onClick={() => handleClick(2)}
+        />
       </div>
 
       <div className='row'>
-        <Button />
-        <Button />
-        <Button />
+        <Button
+          value={squares[3]}
+          onClick={() => handleClick(3)}
+        />
+        <Button
+          value={squares[4]}
+          onClick={() => handleClick(4)}
+        />
+        <Button
+          value={squares[5]}
+          onClick={() => handleClick(5)}
+        />
       </div>
 
       <div className='row'>
-        <Button />
-        <Button />
-        <Button />
+        <Button
+          value={squares[6]}
+          onClick={() => handleClick(6)}
+        />
+        <Button
+          value={squares[7]}
+          onClick={() => handleClick(7)}
+        />
+        <Button
+          value={squares[8]}
+          onClick={() => handleClick(8)}
+        />
       </div>
     </div>
   )
